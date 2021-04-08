152ef0b9446d0cbc99c5b9bb1915e7de87f9239c
Andre Cleaver <andre.cleaver@tufts.edu>
2021-04-07 20:42:45 -0400

    successful vuforia target tracking

---
+++ Vuforia/HololensBuild/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void CubeController::Update()
+extern void CubeController_Update_mAF43CBE184E072D813E5C977C00F87A935F86AEA (void);
+// 0x00000002 System.Void CubeController::.ctor()
+extern void CubeController__ctor_mB129E1351E3150FB6D78F34BBC6BC4B2B954A76C (void);
+static Il2CppMethodPointer s_methodPointers[2] = 
+{
+	CubeController_Update_mAF43CBE184E072D813E5C977C00F87A935F86AEA,
+	CubeController__ctor_mB129E1351E3150FB6D78F34BBC6BC4B2B954A76C,
+};
+static const int32_t s_InvokerIndices[2] = 
+{
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	2,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
